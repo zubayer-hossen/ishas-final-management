45f2de34fcb6d64f0532e16b61e9d1366c231f7c
Zubayer Hossen <fzhossen143r@gmail.com>
2026-07-23 23:45:11 +0600

    seven1.1.1

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/', // <-- এই লাইনটি অবশ্যই রাখুন
   server: {
     port: 5173,
     proxy: {
